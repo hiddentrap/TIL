9f286ed146a6d93cb24b7ea0d0fec5c830b1c259
git_jaejoo <git_jaejoo@jaejoo.iptimer.org>
2020-05-08 13:49:47 +0900

    Add app for servers, with deliberately failing unit test

--- Projects/Django/asset_inventory/servers/tests.py
+++ Projects/Django/asset_inventory/servers/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
 from servers.views import home_page
@@ -6,8 +7,23 @@ from servers.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
+        # Given Nothing
+
         # When
         found = resolve('/')
 
         # Then
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        # Given
+        request = HttpRequest()
+
+        # When
+        response = home_page(request)
+        html = response.content.decode('utf8')
+
+        # Then
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Asset Inventory</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- Projects/Django/asset_inventory/servers/views.py
+++ Projects/Django/asset_inventory/servers/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
 # Create your views here.
-def home_page():
-    pass
+from django.http import HttpResponse
+
+
+def home_page(request):
+    return HttpResponse('<html><title>Asset Inventory</title></html>')
